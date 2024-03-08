a8e85d4f90395f54318d7f84fd84e25db1b0d57c
Jithan Roy <jithanroyjony@gmail.com>
2024-03-08 16:39:28 +0000

    first commit

---
+++ src/shared/widgets/header/header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from "next/link";
+import Logo from "./logo";
+
+const Header = () => {
+  return (
+    <div className='w-full sticky top-0 left-0 z-[999] border-b border-b[#000] px-10 flex items-center justify-between h-[80px] bg-white text-black'>
+        <div>
+          <Link href={"/"}>
+            <Logo/>
+          </Link>
+        </div>
+
+        <div>
+            navitems
+        </div>
+
+        <div className='flex items-center gap-3'>
+          Toolbar
+        </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/shared/widgets/header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      header index
+    </div>
+  );
+};
+
+export default Index;
