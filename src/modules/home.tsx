cc20b135ec5c3acbe64d86e24a4af8d826942d10
Jithan Roy <jithanroyjony@gmail.com>
2024-03-11 21:59:55 +0000

    layout page fix

--- src/modules/home.tsx
+++ src/modules/home.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from "../shared/widgets/header/header";
-import Banner from "./features/banner";
+import Banner from "./home/features/banner";
+import Branding from "./home/features/Branding";
+import Benifits from "./home/features/benifits";
+import FeatureHighlight from "./home/features/feature.highlight";
+import Pricing from "./home/features/pricing";
+import Footer from "../shared/widgets/footer/footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner />
+      <Branding />
+      <Benifits />
+      <FeatureHighlight />
+      <Pricing />
+      <Footer />
     </div>
   );
 };
