d7b25405c68af9dd5a16c9c7d185faaa436b76fa
Jithan <jithanroyjony@gmail.com>
2024-03-25 14:36:35 -0400

    build app

--- src/app/dashboard/new-email/page.tsx
+++ src/app/dashboard/new-email/page.tsx
@@ -1,20 +1,29 @@
-'use client';
+"use client";
 
-import {useSearchParams} from "next/navigation";
+import { ICONS } from "@/shared/utils/icons";
+import dynamic from "next/dynamic";
 import Link from "next/link";
-import {ICONS} from "../../../shared/utils/icons";
-import Emaileditor from "../../../shared/components/editor/email.editor";
+import { useSearchParams } from "next/navigation";
+const Emaileditor = dynamic(
+  () => import("@/shared/components/editor/email.editor"),
+  {
+    ssr: false,
+  }
+);
 
 const Page = () => {
   const searchParams = useSearchParams();
-  const subject: string = searchParams.get('subject')!;
+  const subject: string = searchParams.get("subject")!;
   const subjectTitle = subject.replace(/-/g, " ");
 
   return (
-    <div className='w-full flex bg-[#F7F7F7]'>
-      <div className='w-full p-5 bg-white rounded-r-xl'>
+    <div className="w-full flex bg-[#F7F7F7]">
+      <div className="w-full p-5 bg-white rounded-r-xl">
         {/*back arrow*/}
-        <Link href={"/dashboard/write"} className='opacity-[.7] w-min flex text-xl items-center'>
+        <Link
+          href={"/dashboard/write"}
+          className="opacity-[.7] w-min flex text-xl items-center"
+        >
           <span>{ICONS.backArrow}</span>
           <span> Exit </span>
         </Link>
